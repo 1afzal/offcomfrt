93ffed4bf0e40e299b5d8062727727b3db184c6f
afza1 <afzalali5256@gmail.com>
2026-01-15 13:41:46 +0530

    products added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Landing from "./components/Landing";
+import MiniProduct from './components/MiniProduct';
 
 function App(){
   return(
     <>
       <Landing/>
+      <MiniProduct />
     </>
   )
 }
